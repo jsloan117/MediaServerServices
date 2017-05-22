075a564b503863d701a5e966bd366c91b7ac7170
Jonathan Sloan <jsloan117@gmail.com>
2017-05-22 00:43:26 -0400

    Update subtitle_downloader.sh

--- scripts/subtitle_downloader.sh
+++ scripts/subtitle_downloader.sh
@@ -3,4 +3,4 @@
 
 video_dir="$1"
 
-[[ -n $video_dir && -d $video_dir ]] && subliminal download -l en -v $video_dir || echo -e "Enter directory to run subtitler-downloader against. \n" && exit 1
+[[ -n $video_dir && -d $video_dir ]] && subliminal download -l en -v $video_dir || echo -e "Enter directory to run subtitle-downloader against. \n" && exit 1
